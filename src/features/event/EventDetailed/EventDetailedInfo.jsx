c9c31b6a26d5222156c7c8355101a9da85facc94
keithdidier <keith.b.didier@gmail.com>
2018-11-13 19:39:51 -0500

    creating the event detailed page structure

---
+++ src/features/event/EventDetailed/EventDetailedInfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function EventDetailedInfo() {
+  return (
+    <div>
+      <h1>Info</h1>
+    </div>
+  );
+}
+
+export default EventDetailedInfo;
--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Segment, Item, Icon, List, Button } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 import EventLisAttendee from './EventListAttendee';
 
 class EventListItem extends Component {
@@ -44,8 +45,8 @@ class EventListItem extends Component {
             content="Delete"
           />
           <Button
-            onClick={onEventOpen(event)}
-            as="a"
+            as={Link}
+            to={`/event/${event.id}`}
             color="teal"
             floated="right"
             content="View"
