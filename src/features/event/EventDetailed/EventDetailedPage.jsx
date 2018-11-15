4543521bd877f4e9c38dfa898010d46ab5f5eedb
keithdidier <keith.b.didier@gmail.com>
2018-11-14 21:46:58 -0500

    adding static data for the event detailed page

--- src/features/event/EventDetailed/EventDetailedPage.jsx
+++ src/features/event/EventDetailed/EventDetailedPage.jsx
@@ -5,19 +5,44 @@ import EventDetailedInfo from './EventDetailedInfo';
 import EventDetailedChat from './EventDetailedChat';
 import EventDetailedSidebar from './EventDetailedSidebar';
 
+const event = {
+  id: '1',
+  title: 'Trip to Tower of London',
+  date: '2018-03-27',
+  category: 'culture',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
+  city: 'London, UK',
+  venue: "Tower of London, St Katharine's & Wapping, London",
+  hostedBy: 'Bob',
+  hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
+  attendees: [
+    {
+      id: 'a',
+      name: 'Bob',
+      photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
+    },
+    {
+      id: 'b',
+      name: 'Tom',
+      photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
+    }
+  ]
+};
+
 const EventDetailedPage = () => {
   return (
     <Grid>
-      <Grid>
-        <Grid.Column width={10}>
-          <EventDetailedHeader />
-          <EventDetailedInfo />
-          <EventDetailedChat />
-        </Grid.Column>
-        <Grid.Column width={6}>
-          <EventDetailedSidebar />
-        </Grid.Column>
-      </Grid>
+      <Grid.Column width={10}>
+        <EventDetailedHeader event={event} />
+        <br />
+        <EventDetailedInfo event={event} />
+        <br />
+        <EventDetailedChat />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <EventDetailedSidebar attendees={event.attendees} />
+      </Grid.Column>
     </Grid>
   );
 };
--- src/features/event/EventDetailed/EventDetailedSidebar.jsx
+++ src/features/event/EventDetailed/EventDetailedSidebar.jsx
@@ -1,11 +1,49 @@
 import React from 'react';
+import { Segment, List, Label, Item } from 'semantic-ui-react';
 
-function EventDetailedSidebar() {
+const EventDetailedSidebar = ({ attendees }) => {
+  const isHost = false;
   return (
     <div>
-      <h1>Sidebar</h1>
+      <div>
+        <Segment
+          textAlign="center"
+          style={{ border: 'none' }}
+          attached="top"
+          secondary
+          inverted
+          color="teal"
+        >
+          {attendees && attendees.length}{' '}
+          {attendees && attendees.length === 1 ? 'Person' : 'People'} Going
+        </Segment>
+        <Segment attached>
+          <List relaxed divided>
+            {attendees &&
+              attendees.map(attendee => (
+                <Item key={attendee.id} style={{ position: 'relative' }}>
+                  {isHost && (
+                    <Label
+                      style={{ position: 'absolute' }}
+                      color="orange"
+                      ribbon="right"
+                    >
+                      Host
+                    </Label>
+                  )}
+                  <Item.Image size="tiny" src={attendee.photoURL} />
+                  <Item.Content verticalAlign="middle">
+                    <Item.Header as="h3">
+                      <a>{attendee.name}</a>
+                    </Item.Header>
+                  </Item.Content>
+                </Item>
+              ))}
+          </List>
+        </Segment>
+      </div>
     </div>
   );
-}
+};
 
 export default EventDetailedSidebar;
